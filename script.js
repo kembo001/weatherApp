3b66599bd788059d52cebe71dea2ce9c9d12794f
kembo001 <kemboi2000@outlook.com>
2021-10-08 21:49:56 -0500

    made changes to the js file and added API

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-// fetch
-// fetch("https://community-open-weather-map.p.rapidapi.com/find?q=london&cnt=0&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-// 		"x-rapidapi-key": "3b927d5230mshbfb31e7e4a7d896p147e62jsn7142af571498"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
-
-
-
 
 // selectors 
 var API_KEY = "09243ce323687d1e2cca04c05b950491";
-var date = document.getElementById('date');
-var city = document.getElementById('city');
+// var date = document.getElementById('date');
+// var city = document.getElementById('city');
 var temp = document.getElementById('temp');
-var wind = document.getElementById('wind');
-var humidity = document.getElementById('humidity');
+// var wind = document.getElementById('wind');
+// var humidity = document.getElementById('humidity');
 var uxIndex = document.getElementById('uxIndex');
 var searchBar = document.getElementById('searchBar');
 var btn = document.getElementById('btn');
@@ -33,48 +16,45 @@ var uxIndex = document.getElementById('uxIndex');
 
 
 // functions
-function showWeatherData(data){
-
-}
-// getWeatherData()
-// function getWeatherData(city){
-//     navigator.geolocation.getCurrentPosition((success) =>{
-//         console.log(success);
-
-//         // var {latitude, longitude} = success.coords;
-//         fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city 
-//         +"&units=metric&appid=" 
-//         +  API_KEY)
-//         .then(response => response.json())
-//         .then((data) => {
-
-//             console.log(data)
-//             showWeatherData(data);
-//         })
-//     })
+// function showWeatherData(data){
+//     const { name } = data;
+//     const { icon, description } = data.weather[0];
+//     const { temp, humidity} = data.main;
+//     const { speed } = data.wind
+//     console.log(name, icon, description, temp, humidity, speed )
 // }
 
-function getWeatherData(){
+// function getWeatherData(inputCity){
+//     var inputCity = searchBar.value
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${API_KEY}&units=imperial`)
+//         .then(response => response.json())
+//         .then((showWeatherData) => {
+//         })
+//         // showWeatherData()
+// } 
+function getWeatherData(inputCity){
     var inputCity = searchBar.value
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${API_KEY}&units=imperial`)
-        .then(response => response.json())
-        .then((data) => {
+        .then(weather => {
+            return weather.json();
+        }).then(showWeatherData);
+} 
+function showWeatherData(weather){
+    console.log(weather)
+    let city = document.getElementById('city');
+    // let date = document.getElementById('date');
+    let wind = document.querySelector('.wind');
+    let temp = document.querySelector('.temp');
+    let humidity = document.querySelector('.humidity');
+    city.innerHTML = `${weather.name}`;
+    temp.innerHTML = `${weather.main.temp}°F`
+    wind.innerHTML = `${weather.wind.speed} MPH`
+    humidity.innerHTML = `${weather.main.humidity}%`
 
-            console.log(data)
-            // showWeatherData(data);
-            //within here perform another fetch request to the OneCall API in the weather documentation
-
-        })
 }
+
 
 btn.addEventListener("click", getWeatherData)
 
-// function showWeatherData(data){
-    
-// }
-// getWeatherData()
-// function getWeatherData(){
-//     fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}").then((response)=> response.json())
-//     .then((data) => console.log(data));
-// }
+
 // execute code
